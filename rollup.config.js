c1c1814443ce142fde5069a3d05a8e5808b22286
Stepan Kosatyi <stepan@kosatyi.com>
2023-11-04 03:30:32 +0200

    update

--- rollup.config.js
+++ rollup.config.js
@@ -1,3 +1,5 @@
+import { promises as fs } from 'fs'
+
 export default {
     input: 'index.js',
     output: [
@@ -11,4 +13,18 @@ export default {
         },
     ],
     external: ['fs', 'path', 'glob', 'terser', '@kosatyi/ejs', '@babel/core'],
+    plugins: [
+        {
+            name: 'jsonFile',
+            async buildEnd(err) {
+                if (!err) {
+                    await fs.mkdir('dist/cjs', { recursive: true })
+                    await fs.writeFile(
+                        'dist/cjs/package.json',
+                        JSON.stringify({ type: 'commonjs' }, null, 4)
+                    )
+                }
+            },
+        },
+    ],
 }
